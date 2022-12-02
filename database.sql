b8d313d0199a859fc1a981c4574cd124f5061e52
mobak88 <mostafa0401@ymail.com>
2022-12-02 19:48:18 +0100

    Creating default recipes json

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE recipes_system;
 CREATE TABLE recipe(
     recipe_id SERIAL PRIMARY KEY,
     recipe_name VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL
+    category VARCHAR(255) CHECK (category in ('free', 'premium')) NOT NULL
 );
 
 CREATE TABLE ingredient(
@@ -16,7 +16,11 @@ CREATE TABLE ingredient(
 CREATE TABLE step(
     step_id SERIAL PRIMARY KEY,
     fk_recipe INT REFERENCES recipe (recipe_id) NOT NULL,
-    step_name VARCHAR(255) NOT NULL,
-    UNIQUE(recipe_id)
+    step_text VARCHAR(255) NOT NULL
 );
 
+insert into recipe (recipe_name, category) values ('Easy Pancakes', 'free');
+
+insert into ingredient (fk_recipe, ingredient_name, ingredient_category) values (1, '100g plain flour', 'flour');
+
+insert into step (fk_recipe, step_text) values (1, 'Put 100g plain flour, 2 large eggs, 300ml milk, 1 tbsp sunflower or vegetable oil and a pinch of salt into a bowl');
--- src/app.js
+++ src/app.js
@@ -4,12 +4,48 @@ const app = express();
 const cors = require('cors');
 const pool = require('./db');
 
+const defaultFreeRecipes = require('./defaultFreeRecipes.json');
+
 const url = process.env.URL || 'localhost';
 const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
 app.use(express.json()); // Access to req.body
+
+app.get('/recipes/create/default', async (req, res) => {
+    try {
+        /* for (let i = 0; i < defaultFreeRecipes.length; i++) {
+            await pool.query(
+                'INSERT INTO recipe (recipe_name, category) VALUES($1, $2) RETURNING *',
+                [defaultFreeRecipes[i].name, defaultFreeRecipes[i].category]
+            );
+        } */
+
+        const [ingredients] = defaultFreeRecipes.ingredients;
+
+        /* for (let i = 0; i < ingredients.length; i++) {
+            await pool.query(
+                'INSERT INTO ingredient (fk_recipe, ingredient_name, ingredient_category) VALUES($1, $2, $3) RETURNING *',
+                [defaultFreeRecipes[i].name, defaultFreeRecipes[i].category]
+            );
+        } */
+
+        res.json(ingredients);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.get('/recipes', async (req, res) => {
+    try {
+        const recipes = await pool.query('SELECT * FROM recipe JOIN ingredient ON recipe_id = fk_recipe');
+
+        res.json(recipes.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 app.listen((port), () => {
     console.log(`App running on: http://${url}:${port}`);
