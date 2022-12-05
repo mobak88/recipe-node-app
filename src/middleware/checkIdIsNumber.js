2dc684bed96fec1f6fa11da8851edecde71308d5
mobak88 <mostafa0401@ymail.com>
2022-12-05 21:02:37 +0100

    Tweaking check id is num middleware

--- src/middleware/checkIdIsNumber.js
+++ src/middleware/checkIdIsNumber.js
@@ -6,7 +6,11 @@
 const checkIdIsNumber = (req, res, next) => {
     const { recipe_id, step_id } = req.params;
 
-    if (isNaN(parseInt(recipe_id)) || step_id.length > 0 && isNaN(parseInt(step_id))) {
+    if (step_id && isNaN(parseInt(step_id))) {
+        return res.status(422).json('Please use number');
+    }
+
+    if (isNaN(parseInt(recipe_id))) {
         return res.status(422).json('Please use number');
     } else {
         return next();
