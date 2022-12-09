a670ba127f6331f093808cbbabb75a2f9cd2bf84
mobak88 <mostafa0401@ymail.com>
2022-12-09 13:56:28 +0100

    Checking which user is logged in in logiController

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -11,7 +11,11 @@ exports.login = ('/login', async (req, res) => {
         const { user_type } = req.body;
         const user = req.cookies.user_type;
 
-        if (user === admin || user === premium) {
+        if (user_type === admin && user === admin) {
+            return res.status(401).json('Already logged in');
+        }
+
+        if (user_type === premium && user === premium) {
             return res.status(401).json('Already logged in');
         }
 
@@ -22,7 +26,7 @@ exports.login = ('/login', async (req, res) => {
             res.cookie(`user_type`, `${user_type}`);
             return res.json(`${admin} logged in`);
         } else {
-            return res.status(401).json("Wrong user");
+            return res.status(401).json("Invalid or missing credentials");
         }
 
     } catch (err) {
